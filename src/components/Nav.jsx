35454c0f8304d9ff6ce1900b3594655cfe8bafa0
ferutizi <fer.utizi@gmail.com>
2022-11-29 18:13:23 -0300

    adding mixin button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,17 @@
 import { ThemeContext } from '../context/ThemeContext';
 import { useContext } from 'react';
-import styles from './Nav.module.scss';
+import nav from './Nav.module.scss';
 
 const Nav = () => {
     const { theme } = useContext(ThemeContext);
 
     return(
-        <div className={`${styles.nav__container} ${styles[theme]}`}>
-            <p className={`${styles.section} ${styles[theme]}`}>section 1</p>
-            <p className={`${styles.section} ${styles[theme]}`}>section 2</p>
-            <p className={`${styles.section} ${styles[theme]}`}>section 3</p>
-            <p className={`${styles.section} ${styles[theme]}`}>section 4</p>
-            <button>home</button>
+        <div className={`${nav.nav__container} ${nav[theme]}`}>
+            <p className={`${nav.section} ${nav[theme]}`}>section 1</p>
+            <p className={`${nav.section} ${nav[theme]}`}>section 2</p>
+            <p className={`${nav.section} ${nav[theme]}`}>section 3</p>
+            <p className={`${nav.section} ${nav[theme]}`}>section 4</p>
+            <button className={`${nav.button} ${nav[theme]}`}>home</button>
         </div>
     )
 }
