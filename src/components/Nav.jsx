4ccf24219fe991712c194fb7e4cb924919ac3b98
ferutizi <fer.utizi@gmail.com>
2022-11-29 17:52:59 -0300

    styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,10 +4,10 @@ import styles from './Nav.module.scss';
 
 const Nav = () => {
     const { theme } = useContext(ThemeContext);
-    
+
     return(
         <div className={`${styles.nav__container} ${styles[theme]}`}>
-            <p className={`${styles.section} ${styles[theme]}`}>seccion 1</p>
+            <p className={`${styles.section} ${styles[theme]}`}>section 1</p>
             <p className={`${styles.section} ${styles[theme]}`}>section 2</p>
             <p className={`${styles.section} ${styles[theme]}`}>section 3</p>
             <p className={`${styles.section} ${styles[theme]}`}>section 4</p>
