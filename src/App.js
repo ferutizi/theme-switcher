92555c3f9974ea765d2f7c76c2d6dd676707cd42
ferutizi <fer.utizi@gmail.com>
2022-11-29 15:30:51 -0300

    changeTheme function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const  [theme, setTheme] = useState('dark');
+
+  const changeTheme = () => {
+    theme === 'dark' ? setTheme('light') : setTheme('dark');
+    console.log(theme)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <h1 className={theme}>Theme Switcher</h1>
+        <button onClick={() => changeTheme(theme)}>{theme === 'dark' ? 'light' : 'dark'}</button>
+      </div>
+    </>
   );
 }
 
